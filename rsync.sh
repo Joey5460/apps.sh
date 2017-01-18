5c4f41d679de30716fb91a44bc0f83891ce2ee6a
zhouyu <zhouyu5460@gmail.com>
2017-01-18 18:07:22 +0800

    changed rsync.sh

--- rsync.sh
+++ rsync.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #push
 if [[ $# -eq 0 ]] ; then
-    rsync -azvP --delete ~/Downloads/anywhere/review/ 'userver@192.157.226.233':~/anywhere
+    rsync -azvP --delete ~/Downloads/anywhere/elts 'userver@192.157.226.233':~/anywhere
     exit 0
 fi
 
@@ -9,7 +9,7 @@ while [[ $# -gt 0 ]]
 do
 case $key in
     pull)
-        rsync -azvP 'userver@192.157.226.233':~/anywhere  ~/rsync_repo/anywhere/  
+        rsync -azvP 'userver@192.157.226.233':~/anywhere  ~/repositories/anywhere/  
         ;;
     *)
         ;;
