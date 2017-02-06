632ef83215df59c43879cf5864f7c46bfa3497ca
zhouyu <zhouyu5460@gmail.com>
2017-02-06 17:30:36 +0800

    modified rsync.sh

--- rsync.sh
+++ rsync.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 #push
 if [[ $# -eq 0 ]] ; then
-    rsync -azvP --delete ~/Downloads/anywhere/elts/ 'userver@192.157.226.233':~/anywhere/elts
     exit 0
 fi
 
 while [[ $# -gt 0 ]]
 do
+key="$1"
 case $key in
     pull)
         rsync -azvP 'userver@192.157.226.233':~/anywhere  ~/repositories/anywhere/  
+        ;;
+    usb)
+        rsync -azvP --delete ~/Downloads/anywhere/elts/ /run/media/zhouyu/backup  
+        ;;
+    s)
+        rsync -azvP --delete ~/Downloads/anywhere/elts/ 'userver@192.157.226.233':~/anywhere/elts
         ;;
     *)
         ;;
