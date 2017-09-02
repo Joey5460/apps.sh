0f7a32a82c913efe2b8e93af65e9202bfcdf15ac
fossy <zhouyu5460@gmail.com>
2017-09-02 10:37:31 +0800

    add pip in user config script

--- usercfg.sh
+++ usercfg.sh
@@ -5,10 +5,17 @@ if [ -x "$(command -v apt)" ]; then
    yum_cmd="apt"
 elif [ -x "$(command -v dnf)" ]; then
    echo "fedora-like"
+   yum_cmd="dnf"
 fi 
    #eval $yum_cmd -v 
-   $yum_cmd install git
-   git config --global user.email "zhouyu5460@gmail.com"
-   git config --global user.name "fossy"
+   #$yum_cmd install git
+   #git config --global user.email "zhouyu5460@gmail.com"
+   #git config --global user.name "fossy"
    #echo "alias vi=vim" >> .bashrc
    #git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+   if ! [ -x "$(command -v pip)" ]; then
+     $yum_cmd install pip
+ else
+     echo "pip installed"
+   fi
+   
