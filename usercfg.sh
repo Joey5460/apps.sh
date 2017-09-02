d0f31e247a34c78ba91d6849d2595887400a4ccf
fossy <zhouyu5460@gmail.com>
2017-09-02 11:02:48 +0800

    add shadowsocks

--- usercfg.sh
+++ usercfg.sh
@@ -7,15 +7,19 @@ elif [ -x "$(command -v dnf)" ]; then
    echo "fedora-like"
    yum_cmd="dnf"
 fi 
-   #eval $yum_cmd -v 
-   #$yum_cmd install git
-   #git config --global user.email "zhouyu5460@gmail.com"
-   #git config --global user.name "fossy"
-   #echo "alias vi=vim" >> .bashrc
-   #git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-   if ! [ -x "$(command -v pip)" ]; then
-     $yum_cmd install pip
- else
-     echo "pip installed"
-   fi
+
+#eval $yum_cmd -v 
+####Config Git#####
+$yum_cmd install git
+git config --global user.email "zhouyu5460@gmail.com"
+git config --global user.name "fossy"
+echo "alias vi=vim" >> .bashrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+if ! [ -x "$(command -v pip)" ]; then
+    $yum_cmd install pip
+else
+    echo "pip installed"
+fi
+pip install shadowsocks
    
